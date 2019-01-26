df8d87cdb55a66bc36717d0d8f5f97ac1629415c
alex3223 <shmonka3223@gmail.com>
2019-01-26 20:17:56 +0300

    js-simple-3

--- main.js
+++ main.js
@@ -1,55 +1,82 @@
-//Выведите в консоль любую строку. Если выводимая строка пустая - отобразите строку текста по умолчанию.
-var String = "This is string"
-newString = String || 'Default string';
-console.log(newString);
-//Создайте любой объект, например "браузер" и вывидете в консоль все его поля. + //Cоздать метод для этого объекта
-var Browser = {
-  name: "Google",
-  version: 8.1,
-  rank: 1,
-  sayHi: function(){
-    return 'This is method?)';
-  }
+//Cчётчик замыкания и число в качестве аргумента 
+
+var counter =  (function () {
+  var count = 0;
+
+  return function (num) {
+    count = num !== undefined ? num : count;
+    return count++;
+  };
+}());
+
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter(100));
+console.log(counter());
+console.log(counter());
+console.log(counter(0));
+console.log(counter());
+console.log(counter());
+
+// Cоздайте несколько однотипных объектов и определите для них метод используя ключевое слово this
+
+var greet = function() {
+  return 'This car is ' + this.colour;
 };
-console.log(Browser.name);
-console.log(Browser.version);
-console.log(Browser.rank);
-console.log(Browser.sayHi());
-//Cделайте цикл for от 1 до 20 и выведите в консоль только чётные значения
-for (var i = 1; i < 20; i++){
-  if (i % 2 == 0){
-    console.log(i);
+var car = {
+  name: 'Subaru',
+  colour: 'Blue',
+  greet: greet
+}
+
+var oneMoreCar = {
+  name: 'Kia',
+  colour: 'Silver',
+  greet: greet
+}
+console.log(car.greet())
+console.log(oneMoreCar.greet());
+
+// Добавьте к объекту новое свойство, используя геттер и сеттер 
+
+var greet = function() {
+  return 'This car is ' + this.colour;
+};
+var car = {
+  name: 'Subaru',
+  colour: 'Blue',
+  _mileage: 20000,
+  get miliage(){
+    return this._mileage;
+  },
+  set miliage(value){
+    this._mileage = value < 0 ? 0 : value ;
   }
 }
-//Создайте любой код, используя конструкцию switch/case/default
-var Name = 'Alex',
-  Status;
-  switch(Name){
-    case 'Tom': Status = 'Driver'; break;
-    case 'Sam': Status = 'Manager'; break;
-    default: Status = 'still nobody';
+
+var oneMoreCar = {
+  name: 'Kia',
+  colour: 'Silver',
+  mile: 10000,
+   get mile(){
+    return this._mile;
+  },
+  set mile(value){
+    this._mile = value < 0 ? 0 : value ;
   }
-  console.log(Name + ' ' + Status);
-  //Создайте условие, используя тернарный оператор ?
-  var a = 2;
-  var b = 1;
-  if (a === b){
-    console.log('a равно b') }
-    else {
-      console.log('a не равно b')
-    }
-// Выводите значение переменной, пока не достигнуто значение 7 (используя цикл while)
-var i = 0;
-while (i <= 7){
-  console.log(i);
-  i++;
 }
-// Функция с callback
-function myArg (arg1, arg2, callback){
-  console.log("Число " + arg1 + " в степени " + arg2);
-  var arg3 =  (Math.pow(arg1, arg2));
-  callback(arg3);
-}
-myArg(2, 4, function(arg3){
-  console.log("Результат " + arg3);  
-});
+
+car.miliage = -20;
+console.log(car.miliage);
+car.miliage = 0;
+console.log(car.miliage);
+car.miliage = 2000;
+console.log(car.miliage);
+
+oneMoreCar.mile = -10;
+console.log(oneMoreCar.mile);
+oneMoreCar.mile = 0;
+console.log(oneMoreCar.mile);
+oneMoreCar.mile = 1000;
+console.log(oneMoreCar.mile);
