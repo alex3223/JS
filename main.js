891b20055028419a5d75c33b2e8536a9ec430256
alex3223 <shmonka3223@gmail.com>
2019-01-31 19:16:42 +0300

    2 task of 4 exercise was fixed

--- main.js
+++ main.js
@@ -14,13 +14,49 @@ alert(positiveString);
 // Отобразите текущую дату и время в следующих форматах
 // а)
 
-var today = new Date(). toISOString();
-alert( today );
+var dateA = function(date) {
+  var yy = date.getFullYear();
 
+  var mm = date.getMonth() + 1;
+  if(mm < 10) mm = '0' + 1;
+
+  var dd = date.getDate();
+  if(dd < 10) dd = '0' + dd;
+
+  var hh = date.getHours();
+  if(hh < 10) hh = '0' + hh;
+
+  var min = date.getMinutes();
+  if(min < 10) min = '0' + min;
+
+  return  yy + '-' + mm + '-' + dd + ', ' + hh + ':' +min;
+
+}
+var now = new Date();
+alert(dateA(now));
 // b)
 
-var day = new Date();
-alert (day);
+var dateB = function(date) {
+  var yy = date.getFullYear();
+
+  var mm = date.getMonth() + 1;
+  if(mm < 10) mm = '0' + 1;
+
+  var dd = date.getDate();
+  if(dd < 10) dd = '0' + dd;
+
+  var hh = date.getHours();
+  if(hh < 10) hh = '0' + hh;
+
+  var min = date.getMinutes();
+  if(min < 10) min = '0' + min;
+
+  var sec = date.getSeconds();
+  if(sec < 10) sec = '0' + sec;
+
+  return dd + '.' + mm + '.' + yy + ', ' + hh + ':' + min + ':' + sec;
+}
+alert(dateB(now));
 
 //getWeekDay
 
